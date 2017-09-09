b41fad6810a639c0721225ac3f562c61984e053b
Pedro Acosta Pilataxi <pedro@pcost8300.pcost8300>
2017-09-09 09:53:25 -0500

    Commit inicial

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+    <title>Inicio</title>
+  </head>
+  <body>
+    <div class="container">
+      <form>
+        <div class="form-group">
+          <input type="text" placeholder="Nombre" name="nom" id="nom" value="" class="form-control">
+        </div>
+      </form>
+    </div>
+  </body>
+</html>
