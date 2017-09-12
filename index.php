d85d094ca15a9a2f46299567da7bce8151dc6e58
Pedro Acosta Pilataxi <pedro@pcost8300.pcost8300>
2017-09-12 14:42:52 -0500

    14:42 12/09/2017

--- index.php
+++ index.php
@@ -2,30 +2,55 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script> -->
+    <!-- <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script> -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="css/style.css">
     <link href="https://fonts.googleapis.com/css?family=Signika" rel="stylesheet">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
     <title>Inicio</title>
-    <script>
-      $(function(){
-        $("#boton").click(function(){
-          var nombre = $("#nom").val();
-          var apellido = $("#ape").val();
-          $.post("php/conectar.php",{nom:nombre, ape:apellido},function(data){
+    <!-- <script>
+    $(function(){
+      $("#button").click(function(){
+        var nombre = $("#nom").val();
+        var apellido = $("#ape").val();
+        $.post('php/reg.php',{nom:nombre, ape:apellido}, function(data){
+          if (data != ''){
+            // $("#nom").value(data);
             $("#prueba").html(data);
-          });
+            // document.getElementById("nom").value = data;
+            // setTimeout(function(){message_status.hide()},1000);
+          }
         });
       });
+    });
+    </script> -->
+    <script language="javascript" src="js/jquery-1.9.1.min.js"></script>
+    <script language="javascript" src="js/jquery-1.4.2.min.js"></script>
+    <script language="javascript">
+    $(document).ready(function() {
+      $('#form').submit(function() {
+        $.ajax({
+          type: 'POST',
+          url: $(this).attr('action'),
+          data: $(this).serialize(),
+          success: function(data) {
+            $('#alertas').html(data);
+            document.getElementById("nom").value = "";
+            document.getElementById("ape").value = "";
+          }
+        })
+        return false;
+      });
+    });
     </script>
   </head>
   <body class="body titulo">
     <div class="container translayer" style="margin-top: 15%;">
+      <div id="alertas"></div>
       <h1 class="titulo" style="text-align: center;">Crea tu cédula ficticia</h1>
-      <form>
+      <form id="form" action="php/reg.php" method="POST">
         <div class="row">
           <div class="col">
             <div class="form-group">
@@ -44,20 +69,28 @@
           <div class="col">
             <label for="">Genero:</label>
           </div>
+          <div class="col">
+            <label for="">Correo:</label>
+          </div>
         </div>
         <div class="row">
           <div class="col">
-            <select class="custom-select">
+            <select class="custom-select" name="sexo" id="sexo">
               <option selected value="1">Masculino</option>
-              <option value="2">Femenino</option>
+              <option value="0">Femenino</option>
             </select>
+          </div>
+          <div class="col">
+            <div class="form-group">
+              <input type="email" name="correo" class="form-control" placeholder="chichico@chichi.co" value="">
+            </div>
           </div>
         </div>
         <br>
         <div class="row">
           <div class="col">
             <div class="form-group">
-              <input type="submit" name="boton" id="boton" class="btn btn-primary" value="Enviar">
+              <button type="submit" id="button" class="btn btn-primary" name="button">Aceptar</button>
             </div>
           </div>
         </div>
--- php/reg.php
+++ php/reg.php
@@ -1,14 +1,27 @@
 <?php
 include ("conectar.php");
-guardar($db);
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+  if (!empty($_POST['nom'])) {
+    if (!empty($_POST['ape'])) {
+      guardar($db);
+    }
+  }
+}
 
 
 function guardar($db){
-  $sql = "INSERT INTO usuarios (cedula, nombre, apellido, sexo) values('1231231', 'Pedro', 'Acosta', True)";
+  $sql = "INSERT INTO usuarios (cedula, nombre, apellido, sexo) values('1231231', '".$_POST['nom']."', '".$_POST['ape']."', ".$_POST['sexo'].")";
   if (mysqli_query($db,$sql)){
-    echo "Listo";
+    echo '
+    <div class="alert alert-warning alert-dismissible fade show" role="alert">
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+  </button>
+  <strong>Holy guacamole!</strong> You should check in on some of those fields below.
+</div>
+';
   }else {
-    echo "Valio mierda";
+    echo "Valio";
   }
 }
 
